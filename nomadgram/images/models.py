f826d03f500626d43615714dab14f754f6d88a2b
parkminho <minho4377@daum.net>
2018-08-01 16:00:59 +0900

    fourth

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -13,21 +13,21 @@ class TimeStampedModel(models.Model):
 
 
 class Image(TimeStampedModel):
-
+    
     file = models.ImageField()
     locations = models.CharField(max_length=140)
     caption = models.TextField()
-    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True,  related_name='images')
 
 
 class Comment(TimeStampedModel):
 
     message = models.TextField()
-    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
-    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True, related_name='comments')
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True,  related_name='comments')
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True, related_name='comments')
 
 
 class Like(TimeStampedModel):
 
-    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
-    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True, related_name='likes')
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True, related_name='likes')
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True, related_name='likes')
