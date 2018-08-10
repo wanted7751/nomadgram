e198a1d84c3e00bc8f0dea7f581a1a1c111dc837
parkminho <minho4377@daum.net>
2018-08-11 01:27:06 +0900

    #1-44 Restricting Likes

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -36,25 +36,36 @@ class Feed(APIView):
 
  
 class LikeImage(APIView):
-    
+
     def get(self, request, image_id, format=None):
-        
+
         user = request.user
 
         try:
             found_image = models.Image.objects.get(id=image_id)
         except models.Image.DoesNotExist:
-            return Response(status=404)
-        
-        new_like = models.Like.objects.create(
-            creator=user,
-            image=found_image
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-        )
+        try:
+            preexisting_like = models.Like.objects.get(
+                creator=user,
+                image=found_image
+            )
+            preexisting_like.delete()
 
-        new_like.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
 
-        return Response(status=200)
+        except models.Like.DoesNotExist:
+
+            new_like = models.Like.objects.create(
+                creator=user,
+                image=found_image
+            )
+
+            new_like.save()
+
+            return Response(status=status.HTTP_201_CREATED)
+
 
 
         
