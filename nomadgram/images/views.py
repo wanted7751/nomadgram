6e6ee047685c3431f477add92b7cef3b680aa51c
parkminho <minho4377@daum.net>
2018-08-16 02:15:00 +0900

    #1-56 Searching by Hashtag part Two

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -132,4 +132,17 @@ class Search(APIView):
 
       hashtags = request.query_params.get('hashtags', None)
 
-      print(hashtags)
+      if   hashtags is not None:
+
+            hashtags = hashtags.split(",")
+
+            images = models.Image.objects.filter(
+                tags__name__in=hashtags).distinct()
+
+            serializer = serializers.CountImageSerializer(images, many=True)
+
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+      else:
+
+            return Response(status=status.HTTP_400_BAD_REQUEST)
