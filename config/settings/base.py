0607ca85bebc8e94be1302d462d8b7265814c0ee
parkminho <minho4377@daum.net>
2018-08-18 03:44:53 +0900

    #1-58 Creating Notification Models

--- config/settings/base.py
+++ config/settings/base.py
@@ -73,7 +73,9 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     'nomadgram.users.apps.UsersAppConfig',
     # Your stuff: custom apps go here
-    'nomadgram.images.apps.ImagesConfig'
+    'nomadgram.images.apps.ImagesConfig',
+    'nomadgram.notifications.apps.NotificationsConfig'  
+    # notifications app
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
---
+++ nomadgram/notifications/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from . import models
+
+@admin.register(models.Notifications)
+class NotificationsAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'creator',
+        'to',
+        'notification_type'
+    
+        
+    )
