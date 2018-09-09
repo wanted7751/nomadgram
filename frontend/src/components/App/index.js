b15fa36a8906f82189bbbed35785101ee241088f
parkminho <minho4377@daum.net>
2018-09-10 02:46:06 +0900

    #3-16 Moving the App Component and Adding Reset-CSS

---
+++ frontend/src/components/App/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import styles from './styles.scss';
+
+
+class App extends Component {
+    render() {
+        return (
+            <div className={styles.App}>
+                <header className={styles.App__header}>
+                    <h1 className={styleMedia.App__title}>Welcome to React</h1>
+                </header>
+                <p className={styles.App__intro}>
+                    Park min ho
+        </p>
+            </div>
+        );
+    }
+}
+
+export default App;
+
+
+
+
