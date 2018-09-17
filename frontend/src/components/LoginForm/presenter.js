c9dbbfe971c947fe809e2e025b12d63471d30634
parkminho <minho4377@daum.net>
2018-09-17 18:12:33 +0900

    #3-34 Controlling inputs and submit on LoginForm (코딩 챌린지 #1)

--- frontend/src/components/LoginForm/presenter.js
+++ frontend/src/components/LoginForm/presenter.js
@@ -5,12 +5,22 @@ import formStyles from "shared/formStyles.scss";
 
 const LoginForm = props => (
   <div className={formStyles.formComponent}>
-    <form className={formStyles.form}>
-      <input type="text" placeholder="Username" className={formStyles.textInput} />
+    <form className={formStyles.form} onSubmit={props.handleSubmit}>
+      <input
+        type="text"
+        placeholder="Username"
+        className={formStyles.textInput}
+        value={props.usernameValue}
+        onChange={props.handleInputChange}
+        name = "username"
+      />
       <input
         type="password"
         placeholder="Password"
         className={formStyles.textInput}
+        value={props.passwordValue}
+        onChange={props.handleInputChange}
+        name="password"
       />
       <input type="submit" value="Log in" className={formStyles.button} />
     </form>
@@ -22,6 +32,15 @@ const LoginForm = props => (
     <span className={formStyles.forgotLink}>Forgot password?</span>
   </div>
 );
+
+
+
+LoginForm.poptypes = {
+  usernameValue: PropTypes.string.isRequired,
+  passwordValue: PropTypes.string.isRequired,
+  handleInputChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired
+};
 
 
 
