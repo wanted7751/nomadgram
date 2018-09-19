5f536f81b4d70f6c57b25aa3f3e12c710d080306
parkminho <minho4377@daum.net>
2018-09-20 00:16:30 +0900

    #3-38 Setting the Token with FB Login

--- frontend/src/redux/modules/user.js
+++ frontend/src/redux/modules/user.js
@@ -2,7 +2,17 @@
 
 // actions
 
+const SAVE_TOKEN = "SAVE_TOKEN";
+
+
 // action creators
+
+function saveToken(token){
+    return{
+        type: SAVE_TOKEN,
+        token: token
+    };
+}
 
 // API actions
 
@@ -18,7 +28,12 @@ function facebookLogin(access_token) {
             })
         })
             .then(response => response.json())
-            .then(json => console.log(json))
+            .then(json => {
+                if(json.token){
+                    localStorage.setItem("jwt", json.token);
+                    dispatch(saveToken(json.token));
+                }
+            })
             .catch(err => console.log(err));
     };
 }
@@ -26,19 +41,30 @@ function facebookLogin(access_token) {
 // initial state
 
 const initialState = {
-    isLoggedIn: localStorage.getItem("jwt") || false
+    isLoggedIn: localStorage.getItem("jwt") ? true : false
 };
 
 // reducer
 
 function reducer(state = initialState, action) {
     switch (action.type) {
+        case SAVE_TOKEN:
+            return applySetToken(state, action);
         default:
             return state;
     }
 }
 
 // reducer functions
+
+function applySetToken(state, action){
+    const {token} = action;
+    return {
+        ...state,
+        isLoggedIn:true,
+        token
+    };
+}
 
 // exports
 
@@ -50,4 +76,4 @@ export { actionCreators };
 
 // export reducer by default
 
-export default reducer;
+export default reducer ;
