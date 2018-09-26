912de4475ab46b0f7dfc6b137dea403605bb5d86
parkminho <minho4377@daum.net>
2018-09-27 02:34:49 +0900

    #3-45 Creating a reusable Loading Component

--- frontend/src/components/Feed/presenter.js
+++ frontend/src/components/Feed/presenter.js
@@ -1,12 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
-import style from "./styles.scss";
+import styles from "./styles.scss";
+import Loading from "components/Loading";
 
-const Feed = props => "Feed!";
-
-Feed.PropTypes = {
-    loading:PropTypes.bool.isRequired
+const Feed = props => {
+    if (props.loading) {
+        return <LoadingFeed />;
+    }
 };
-
+const LoadingFeed = props => (
+    <div className={styles.feed}>
+        <Loading />
+    </div>
+);
+Feed.propTypes = {
+    loading: PropTypes.bool.isRequired
+};
 
 export default Feed;
