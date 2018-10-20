77169706825cb8eece7c688164cd74152bf6724b
parkminho <minho4377@daum.net>
2018-10-21 01:52:30 +0900

    #3-57 Controlling Comment Component

--- frontend/src/components/PhotoComments/index.js
+++ frontend/src/components/PhotoComments/index.js
@@ -3,8 +3,8 @@ import PropTypes from "prop-types";
 import styles from "./styles.scss";
 
 const PhotoComments = props => (
-    <div>
-        <ul>
+    <div className={styles.comments}>
+        <ul className={styles.list}>
             <Comment username={props.creator} comment={props.caption} />
             {props.comments.map(comment => (
                 <Comment
@@ -16,11 +16,14 @@ const PhotoComments = props => (
         </ul>
     </div>
 );
+
 const Comment = props => (
-    <li>
-        <span>{props.username}</span> <span>{props.comment}</span>
+    <li className={styles.comment}>
+        <span className={styles.username}>{props.username}</span>{" "}
+        <span className={styles.message}>{props.comment}</span>
     </li>
 );
+
 PhotoComments.propTypes = {
     caption: PropTypes.string.isRequired,
     creator: PropTypes.string.isRequired,
@@ -34,4 +37,5 @@ PhotoComments.propTypes = {
         })
     ).isRequired
 };
+
 export default PhotoComments;
