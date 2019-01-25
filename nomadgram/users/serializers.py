7ef01e23e6b5d49fc1df9e90d87b5e5940ba2be5
parkminho <minho4377@daum.net>
2019-01-25 09:32:54 +0900

    user serializer pk add and build file modify

--- nomadgram/users/serializers.py
+++ nomadgram/users/serializers.py
@@ -16,6 +16,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.User
         fields = (
+            'pk',
             'profile_image',
             'username',
             'name',
